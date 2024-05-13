6ab78e001412c581469a31baaa6f4802c1514c70
Subham Kundu <subhamkundu.bankura12@gmail.com>
2024-05-13 20:54:44 +0530

    my avatar

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,46 @@
-import { OrbitControls } from "@react-three/drei";
+import { ContactShadows, Environment, OrbitControls, Sky} from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useControls } from "leva";
 
 export const Experience = () => {
+
+  const {animation} = useControls({
+    animation:{
+      value:"Typing",
+      options: ["Typing","Falling" , "Standing"]
+    }
+
+  })
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+      <Sky/>
+      <Environment preset="sunset"/>
+      <group position-y={-1}>
+        <ContactShadows 
+          opacity={0.45}
+          scale={10}
+          blur={1}
+          far={10}
+          resolution={256}
+          color="#000000"
+        />
+      <Avatar animation={animation}/>
+        {animation === "Typing" && (
+                <mesh scale={[0.8,0.5,0.8]}  position-y={0.25}>
+                <boxGeometry/>
+                <meshStandardMaterial color="white"/>
+              </mesh>
+        )}
+
+
+      <mesh scale={5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
+        <planeGeometry/>
+        <meshStandardMaterial color="white"/>
       </mesh>
+      
+      <ambientLight intensity={0.1}/>
+      </group>
     </>
   );
 };
